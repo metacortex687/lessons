c68f93ba650cc4f871adef0bbc1181aca07b7375
Alexey Osipov <metacortex687@gmail.com>
2025-07-18 16:21:13 +0700

    Задание 11

---
+++ DSA1/task11-3.py
@@ -0,0 +1,129 @@
+# сделал как добавление новго фильтра к текущему
+# реализовал через битовую операцию между значениями фильтра, 
+# тогда битовая маска будет содержать для всех значений
+
+
+# Фильтр Блюма, он со 100% вероятностью отвечает на вопрос если значение не помещалось в фильтр. 
+# Тоесть ложно отрицательных значений нет.
+# При удалении возможно, что удалятся биты от других значений и значит появятся ложно отрицательные срабатывания. 
+# Что необходимо избежать. 
+# а
+
+
+
+import unittest
+from task11 import BloomFilter
+
+#python ./DSA1/task11-3.py
+
+import importlib.util
+import sys
+spec = importlib.util.spec_from_file_location("task11-2", "./DSA1/task11-2.py")
+task11_2 = importlib.util.module_from_spec(spec)
+sys.modules["task11-2"] = task11_2
+spec.loader.exec_module(task11_2)
+BloomFilter2 = task11_2.BloomFilter2
+
+class TestBloomFilter(unittest.TestCase):
+    def test_simply(self):
+        bf = BloomFilter(32)
+        self.assertFalse(bf.is_value("0123456789"))
+        bf.add("0123456789")
+        self.assertTrue(bf.is_value("0123456789"))
+        
+        bf.add("12345")
+        self.assertTrue(bf.is_value("0123456789"))    
+        self.assertTrue(bf.is_value("12345")) 
+        
+        _str = "11122111111111111111" 
+        self.assertFalse(bf.is_value("111111111111111"))     
+        
+    def test_many(self):
+        bf = BloomFilter(32)
+        
+        _str = "0123456789"
+        for _ in range(10):
+            bf.add(_str)
+            _str = _str[1:] + _str[0]
+            
+        _str = "0123456789"
+        for _ in range(10):
+            self.assertTrue(bf.is_value(_str))  
+            _str = _str[1:] + _str[0]    
+            
+        _str = "12345" 
+        self.assertFalse(bf.is_value(_str))             
+ 
+        
+class TestBloomFilter2(unittest.TestCase):
+    def test_simply(self):
+        bf = BloomFilter2(32)
+        self.assertFalse(bf.is_value("0123456789"))
+        bf.add("0123456789")
+        self.assertTrue(bf.is_value("0123456789"))
+        
+        bf.add("12345")
+        self.assertTrue(bf.is_value("0123456789"))    
+        self.assertTrue(bf.is_value("12345")) 
+        
+        _str = "11122111111111111111" 
+        self.assertFalse(bf.is_value("111111111111111"))     
+        
+    def test_many(self):
+        bf = BloomFilter2(32)
+        
+        _str = "0123456789"
+        for _ in range(10):
+            bf.add(_str)
+            _str = _str[1:] + _str[0]
+            
+        _str = "0123456789"
+        for _ in range(10):
+            self.assertTrue(bf.is_value(_str))  
+            _str = _str[1:] + _str[0]    
+            
+        _str = "12345" 
+        self.assertFalse(bf.is_value(_str))                
+
+
+    def test_add_filter(self):
+        bf1 = BloomFilter2(32)
+        bf2 = BloomFilter2(32)
+        _val = "0123456789"
+        self.assertFalse(bf1.is_value(_val))
+        self.assertFalse(bf2.is_value(_val))
+        bf2.add("0123456789")
+        bf1.add_filter(bf2)
+        self.assertTrue(bf1.is_value(_val))
+        
+        bf1 = BloomFilter2(32)
+        bf2 = BloomFilter2(32)   
+        
+        _str = "0123456789"
+        for _ in range(5):
+            bf1.add(_str)
+            _str = _str[1:] + _str[0]
+            
+        for _ in range(5,10):
+            bf2.add(_str)
+            _str = _str[1:] + _str[0]           
+        
+        bf2.add_filter(bf1) 
+            
+        _str = "0123456789"
+        for _ in range(10):
+            self.assertTrue(bf2.is_value(_str))  
+            _str = _str[1:] + _str[0]   
+            
+            
+        _str = "11122111111111111111" 
+        self.assertFalse(bf2.is_value("111111111111111"))       
+
+        
+        
+if __name__ == '__main__':
+    unittest.main()
+   
+   
+     
+        
---
+++ DSA1/task11.py
@@ -0,0 +1,38 @@
+class BloomFilter:
+
+    def __init__(self, f_len):
+        self.filter_len = f_len
+        self.filter = 0
+
+    def hash1(self, str1):
+        _mask = (1 << self.filter_len) - 1
+        _hash = 0
+        # 17
+        for c in str1:
+            _hash = _hash*17 + ord(c)
+            _hash &= _mask
+        return _hash
+
+    def hash2(self, str1):
+        _mask = (1 << self.filter_len) - 1
+        _hash = 0
+        # 223
+        for c in str1:
+            _hash = _hash*223 + ord(c)
+            _hash &= _mask
+        return _hash
+
+    def add(self, str1):
+        self.filter |= self.hash1(str1)
+        self.filter |= self.hash1(str1)
+
+
+    def is_value(self, str1) -> bool:
+        
+        _hash1 = self.hash1(str1)
+        _hash2 = self.hash1(str1)
+        
+        return (_hash1 & self.filter == _hash1) and  (_hash2 & self.filter == _hash2)
+    
+    
+        
