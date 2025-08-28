74cdb86d574f71275d5e0fd267ca32e237882162
metacortex687 <metacortex687@gmail.com>
2025-08-29 01:40:59 +0700

    ссылка на тесты

--- Recursion/task1.py
+++ Recursion/task1.py
@@ -1,6 +1,6 @@
 #1. возведение числа N в степень M;
 # алгоритмическая сложность O(M)
-# тесты → task1_test.py: https://github.com/metacortex687/lessons/blob/main/Recursion/task1_test.py
+# тесты → ./task1_test.py: https://github.com/metacortex687/lessons/blob/main/Recursion/task1_test.py
 
 
 def pow_rec(n,m):
