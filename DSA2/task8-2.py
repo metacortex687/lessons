a3da5c624cbd4cde6731ce5f6bb4f9eae1c3827d
metacortex687 <metacortex687@gmail.com>
2025-09-15 21:43:41 +0700

    сигнатура методов приведена к условию

--- DSA2/task8-2.py
+++ DSA2/task8-2.py
@@ -31,65 +31,29 @@ class DirectedGraph:
         self.m_adjacency = [[0] * size for _ in range(size)]
         self.vertex: list[Vertex] = [None] * size
 
-    def AddVertex(self, v):
+    def AddVertex(self, value):
         for i, _v in enumerate(self.vertex):
             if _v is None:
-                self.vertex[i] = Vertex(v)
+                self.vertex[i] = Vertex(value)
                 return
 
     def RemoveVertex(self, v):
-        i = self._find_vertex(v)
-        if i is None:
-            return
-        self.vertex[i] = None
+        self.vertex[v] = None
 
         for j in range(self.max_vertex):
-            self.m_adjacency[i][j] = 0
-            self.m_adjacency[j][i] = 0
+            self.m_adjacency[v][j] = 0
+            self.m_adjacency[j][v] = 0
 
     def IsEdge(self, v1: int, v2: int) -> bool:
-        i1 = self._find_vertex(v1)
-        if i1 is None:
-            return False
-
-        i2 = self._find_vertex(v2)
-        if i2 is None:
-            return False
-
-        return self.m_adjacency[i1][i2] == 1
+        return self.m_adjacency[v1][v2] == 1
 
     def AddEdge(self, v1: int, v2: int):
-        i1 = self._find_vertex(v1)
-        if i1 is None:
-            return
-
-        i2 = self._find_vertex(v2)
-        if i2 is None:
-            return
-
-        self.m_adjacency[i1][i2] = 1
-        self.m_adjacency[i2][i1] = -1
+        self.m_adjacency[v1][v2] = 1
+        self.m_adjacency[v2][v1] = 1
 
     def RemoveEdge(self, v1, v2):
-        i1 = self._find_vertex(v1)
-        if i1 is None:
-            return
-
-        i2 = self._find_vertex(v2)
-        if i2 is None:
-            return
-
-        self.m_adjacency[i1][i2] = 0
-        self.m_adjacency[i2][i1] = 0
-
-    def _find_vertex(self, v: int) -> int | None:
-        for i, _v in enumerate(self.vertex):
-            if _v is None:
-                continue
-            if _v.Value == v:
-                return i
-
-        return None
+        self.m_adjacency[v1][v2] = 0
+        self.m_adjacency[v2][v1] = 0
 
     def IsCyclic(self) -> bool:
         for idx_start, vertex in enumerate(self.vertex):
