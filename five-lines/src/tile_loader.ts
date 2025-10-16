e70998ae4c0bf0227ad0c539cd70c2d566ffa63a
metacortex687 <metacortex687@gmail.com>
2025-10-16 22:58:12 +0700

    refactor: преобразование number в Tile вынесенов в отдельный модуль и класс

---
+++ five-lines/src/tile_loader.ts
@@ -0,0 +1,140 @@
+import { type Tile } from "./tiles.js";
+import {
+  Air,
+  EmptyGround,
+  Garden,
+  Flux,
+  Unbreakable,
+  PlayerTile,
+  Stone,
+  Resting,
+  Falling,
+  Box,
+  KeyLockBundle,
+  Door,
+} from "./tiles.js";
+
+
+interface RawTileValue {
+  transform(): Tile;
+}
+class AirValue implements RawTileValue {
+  transform(): Tile {
+    return new Air(new EmptyGround());
+  }
+}
+class GardenValue implements RawTileValue {
+  transform(): Tile {
+    return new Air(new Garden());
+  }
+}
+
+class FluxValue implements RawTileValue {
+  transform(): Tile {
+    return new Flux();
+  }
+}
+class UnbreakableValue implements RawTileValue {
+  transform(): Tile {
+    return new Unbreakable();
+  }
+}
+class PlayerValue implements RawTileValue {
+  transform(): Tile {
+    return new PlayerTile();
+  }
+}
+class StoneValue implements RawTileValue {
+  transform(): Tile {
+    return new Stone(new Resting());
+  }
+}
+class FallingStoneValue implements RawTileValue {
+  transform(): Tile {
+    return new Stone(new Falling());
+  }
+}
+class BoxValue implements RawTileValue {
+  transform(): Tile {
+    return new Box(new Resting());
+  }
+}
+class FallingBoxValue implements RawTileValue {
+  transform(): Tile {
+    return new Box(new Falling());
+  }
+}
+class Key1Value implements RawTileValue {
+  transform(): Tile {
+    return YELLOW_KEY_LOCK_FACTORY.key();
+  }
+}
+class Lock1Value implements RawTileValue {
+  transform(): Tile {
+    return YELLOW_KEY_LOCK_FACTORY.lock();
+  }
+}
+class Key2Value implements RawTileValue {
+  transform(): Tile {
+    return BLUE_KEY_LOCK_FACTORY.key();
+  }
+}
+class Lock2Value implements RawTileValue {
+  transform(): Tile {
+    return BLUE_KEY_LOCK_FACTORY.lock();
+  }
+}
+
+class DoorValue implements RawTileValue {
+  transform(): Tile {
+    return new Door();
+  }
+}
+
+class RawTile {
+  transform(): Tile {
+    return this.value.transform();
+  }
+  static readonly AIR = new RawTile(new AirValue());
+  static readonly FLUX = new RawTile(new FluxValue());
+  static readonly UNBREAKABLE = new RawTile(new UnbreakableValue());
+  static readonly PLAYER = new RawTile(new PlayerValue());
+  static readonly STONE = new RawTile(new StoneValue());
+  static readonly FALLING_STONE = new RawTile(new FallingStoneValue());
+  static readonly BOX = new RawTile(new BoxValue());
+  static readonly FALLING_BOX = new RawTile(new FallingBoxValue());
+  static readonly KEY1 = new RawTile(new Key1Value());
+  static readonly LOCK1 = new RawTile(new Lock1Value());
+  static readonly KEY2 = new RawTile(new Key2Value());
+  static readonly LOCK2 = new RawTile(new Lock2Value());
+  static readonly DOOR = new RawTile(new DoorValue());
+  static readonly GARDEN = new RawTile(new GardenValue());
+
+  private constructor(private value: RawTileValue) {}
+}
+
+const RAW_TILES = [
+  RawTile.AIR,
+  RawTile.FLUX,
+  RawTile.UNBREAKABLE,
+  RawTile.PLAYER,
+  RawTile.STONE,
+  RawTile.FALLING_STONE,
+  RawTile.BOX,
+  RawTile.FALLING_BOX,
+  RawTile.KEY1,
+  RawTile.LOCK1,
+  RawTile.KEY2,
+  RawTile.LOCK2,
+  RawTile.DOOR,
+  RawTile.GARDEN,
+];
+
+const YELLOW_KEY_LOCK_FACTORY = new KeyLockBundle("#ffcc00");
+const BLUE_KEY_LOCK_FACTORY = new KeyLockBundle("#00ccff");
+
+export class TileLoader {
+  transform(i: number): Tile {
+    return RAW_TILES[i].transform();
+  }
+}
