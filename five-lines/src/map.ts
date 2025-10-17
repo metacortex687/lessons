868a1f28ff9b0e7275ba50d3f82747392dc60f8f
metacortex687 <metacortex687@gmail.com>
2025-10-17 18:21:03 +0700

    refactor: сделан класс Layer для последующего создания интерфейса

--- five-lines/src/map.ts
+++ five-lines/src/map.ts
@@ -13,9 +13,9 @@ let rawMap: number[][] = [
   [2, 2, 2, 2, 2, 2, 2, 2],
 ];
 
-export class GameMap {
-  private map!: Tile[][];
+class Layer {
   private tile_loader: NumberToTileTransformer = new NumberToTileTransformer();
+  private map!: Tile[][];
 
   constructor() {
     this.load_data();
@@ -31,10 +31,6 @@ export class GameMap {
     }
   }
 
-  isAir(y: number, x: number) {
-    return this.map[y][x].isAir();
-  }
-
   removeTile(tile: Tile) {
     for (let y = 0; y < this.map.length; y++) {
       for (let x = 0; x < this.map[y].length; x++) {
@@ -45,41 +41,89 @@ export class GameMap {
     }
   }
 
-  getBlockOnTopState(x: number, y: number) {
-    return this.map[y][x].getBlockOnTopState();
+  private getMap() {
+    return this.map;
+  }
+
+  isAir(y: number, x: number) {
+    return this.getMap()[y][x].isAir();
   }
 
   draw(tr: TileRenderer) {
-    for (let y = 0; y < this.map.length; y++) {
-      for (let x = 0; x < this.map[y].length; x++) {
-        this.map[y][x].draw(tr, x, y);
+    for (let y = 0; y < this.getMap().length; y++) {
+      for (let x = 0; x < this.getMap()[y].length; x++) {
+        this.getMap()[y][x].draw(tr, x, y);
       }
     }
   }
 
-  moveHorizontal(player: Player, x: number, y: number, dx: number) {
-    this.map[y][x + dx].moveHorizontal(player, this, dx);
+  getBlockOnTopState(x: number, y: number) {
+    return this.getMap()[y][x].getBlockOnTopState();
   }
 
-  moveVertical(player: Player, x: number, y: number, dy: number) {
-    this.map[y + dy][x].moveVertical(player, this, dy);
+
+   moveHorizontal(map:GameMap,player: Player, x: number, y: number, dx: number) {
+
+    this.getMap()[y][x + dx].moveHorizontal(player, map, dx);
+  }
+
+  moveVertical(map:GameMap,player: Player, x: number, y: number, dy: number) {
+    this.getMap()[y + dy][x].moveVertical(player, map, dy);
   }
 
   moveTileTo(x: number, y: number, newx: number, newy: number) {
-    this.map[newy][newx] = this.map[y][x];
-    this.map[y][x] = new Air(new EmptyGround());
+    this.getMap()[newy][newx] = this.getMap()[y][x];
+    this.getMap()[y][x] = new Air(new EmptyGround());
   }
 
-  pushHorisontal(player: Player, tile: Tile, x: number, y: number, dx: number) {
+  pushHorisontal(map:GameMap, player: Player, tile: Tile, x: number, y: number, dx: number) {
     if (this.isAir(y, x + dx + dx) && !this.isAir(y + 1, x + dx)) {
-      this.map[y][x + dx + dx] = tile;
-      player.moveToTile(this, x + dx, y);
+      this.getMap()[y][x + dx + dx] = tile;
+      player.moveToTile(map, x + dx, y);
     }
   }
 
+  update(map: GameMap) {
+    for (let y = this.getMap().length - 1; y >= 0; y--)
+      for (let x = 0; x < this.getMap()[y].length; x++)
+        this.getMap()[y][x].update(map, x, y);
+  }
+}
+
+export class GameMap {
+  private layer = new Layer();
+
+  constructor() {}
+
+  getBlockOnTopState(x: number, y: number) {
+    return this.layer.getBlockOnTopState(x,y);
+  }
+
+  draw(tr: TileRenderer) {
+    this.layer.draw(tr);
+  }
+
+  moveHorizontal(player: Player, x: number, y: number, dx: number) {
+    this.layer.moveHorizontal(this,player, x, y, dx);
+  }
+
+  moveVertical(player: Player, x: number, y: number, dy: number) {
+    this.layer.moveVertical(this,player, x, y, dy);
+  }
+
+  moveTileTo(x: number, y: number, newx: number, newy: number) {
+    this.layer.moveTileTo(x,y,newx,newy);
+  }
+
+  pushHorisontal(player: Player, tile: Tile, x: number, y: number, dx: number) {
+    this.layer.pushHorisontal(this,player, tile, x, y, dx);
+  }
+
+  removeTile(tile: Tile) {
+    this.layer.removeTile(tile);
+  }
+
   update() {
-    for (let y = this.map.length - 1; y >= 0; y--)
-      for (let x = 0; x < this.map[y].length; x++)
-        this.map[y][x].update(this, x, y);
+    this.layer.update(this);
   }
 }
