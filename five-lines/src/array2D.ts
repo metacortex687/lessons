583238b1f21e05132816896ce20335ff2a28b7f6
metacortex687 <metacortex687@gmail.com>
2025-10-19 20:29:15 +0700

    refactor: убрал if в методе удаления

--- five-lines/src/array2D.ts
+++ five-lines/src/array2D.ts
@@ -1,6 +1,9 @@
-export class Array2d<T> {
+export class Array2d<T extends object> {
   private map: T[][];
+  private pos: WeakMap<T, { x: number; y: number }>;  
+
   constructor(private size_x: number, private size_y: number) {
+    this.pos = new WeakMap<T, { x: number; y: number }>(); 
     this.map = new Array<T[]>(size_y);
     for (let y = 0; y < size_y; y++) {
       this.map[y] = new Array<T>(size_x);
@@ -9,6 +12,7 @@ export class Array2d<T> {
 
   setValue(x: number, y: number, value: T): void {
     this.map[y][x] = value;
+    this.pos.set(value, { x, y })
   }
 
   getValue(x: number, y: number): T {
@@ -16,12 +20,9 @@ export class Array2d<T> {
   }
 
   removeTile(tile: T, new_tile: T) {
-    for (let y = 0; y < this.map.length; y++) {
-      for (let x = 0; x < this.map[y].length; x++) {
-        if (this.map[y][x] === tile) {
-          this.map[y][x] = new_tile;
-        }
-      }
-    }
+    const p = this.pos.get(tile);
+    this.map[p!.y][p!.x] = new_tile;
+    this.pos.set(new_tile,p!);
+    this.pos.delete(tile);
   }
 }
