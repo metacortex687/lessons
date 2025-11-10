679bf9fee8456101228ae7b9af1cbf7f86b5e4af
metacortex687 <metacortex687@gmail.com>
2025-11-10 20:57:27 +0700

    начало создания модели

--- Django/Rusakov/storedvd/shop/models.py
+++ Django/Rusakov/storedvd/shop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Section(models.Model):
+    title = models.CharField(
+        max_length=70,
+        help_text='Тут надо ввести название раздела',
+        unique=True,
+        verbose_name='Название раздела'
+    )
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.title
+    
+    
