9d44e97f91fdedec47a631b10da0dce09b25628a
metacortex687 <metacortex687@gmail.com>
2025-11-12 20:50:25 +0700

    модель зарегистрирована

--- Django/Rusakov/storedvd/shop/admin.py
+++ Django/Rusakov/storedvd/shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from shop.models import Section,Product,Discount,Order,OrderLine
 
-# Register your models here.
+admin.site.register(Section)
+admin.site.register(Product)
+admin.site.register(Discount)
+admin.site.register(Order)
+admin.site.register(OrderLine)
