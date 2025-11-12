75a5f17c9e45a7d30f18f2e7f6e3be678b5e2ddd
metacortex687 <metacortex687@gmail.com>
2025-11-12 19:33:58 +0700

    уточнена модель

--- Django/Rusakov/blog/blog/models.py
+++ Django/Rusakov/blog/blog/models.py
@@ -1,63 +1,85 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import MinLengthValidator
 
 
-class Author(
-    models.Model
-):  # пользователя не хочу использовать, так как не все пользователи авторы. Автором становишься после написания статьи. Или иных предусловий.
-    nick_name = models.CharField(max_length=50)  # не уверен насколько нужно
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
+# class Author(
+#     models.Model
+# ):  # пользователя не хочу использовать, так как не все пользователи авторы. Автором становишься после написания статьи. Или иных предусловий.
+#     nick_name = models.CharField(max_length=50)  # не уверен насколько нужно
+#     user = models.OneToOneField(
+#         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Автор"
+#     )
 
-    class Meta:
-        verbose_name = "Автор"
-        verbose_name_plural = "Авторы"
-
-
-class Photo(models.Model):
-    describe = models.CharField(max_length=150)
-    path = models.CharField(
-        max_length=150
-    )  # в иидеале должно генерироваться возможно UUIDField и под ним сохранять
-    author = models.OneToOneField(
-        "Author", on_delete=models.CASCADE
-    )  # Решил явно указать кто грузит. Что бы только свои картинки могли использовать
-
-    class Meta:
-        verbose_name = "Фотография"
-        verbose_name_plural = "Фотографии"
+#     class Meta:
+#         verbose_name = "Автор"
+#         verbose_name_plural = "Авторы"
 
 
 class Article(models.Model):
+
     title = models.CharField(
+        validators=[MinLengthValidator(2)],
         max_length=70,
+        verbose_name="Заголовок",
         help_text="Введите название статьи",
-        verbose_name="Название статьи",
-    )
-    authors = models.ManyToManyField(
-        Author, through="ArticleAuthor", through_fields=("article", "author")
     )
 
-    photos = models.ManyToManyField(
-        Photo, through="ArticlePhoto", through_fields=("article", "photo")
-    )  # хочу сделать так, что бы фотография могла быть только в одной статье или быть просто загруженной
+    subtitle = models.CharField(
+        max_length=255, verbose_name="Подзаголовок", blank=True, default=""
+    )
 
-    content = models.TextField()
-    date = models.TimeField()
+    preview = models.CharField(
+        validators=[MinLengthValidator(5)],
+        max_length=300,
+        verbose_name="Превью",
+        help_text="Отображается в списке статей",
+    )
+    content = models.TextField(verbose_name="Текст статьи")
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="Автор")
+
+    photo = models.ImageField(upload_to="images", verbose_name="Изображения")
+
+    date = models.TimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-date"]
+        verbose_name = "Статья"
+        vebose_name_plural = "Статьи"
 
 
 class Comment(models.Model):
-    authors = models.ForeignKey(Author, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author = models.CharField(
+        max_length=70, validators=[MinLengthValidator(1)], verbose_name="Имя"
+    )
+    email = models.EmailField()
+
     content = models.TextField()
+    date = models.TimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-date"]
+        verbose_name = "Комментарий"
+        vebose_name_plural = "Комментарии"
 
 
-class ArticlePhoto(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
+class Feedback(models.Model):
+    author = models.CharField(
+        max_length=70, validators=[MinLengthValidator(1)], verbose_name="Имя"
+    )
+    email = models.EmailField()
+    content = models.TextField()
+    date = models.TimeField(auto_now_add=True)
 
+    STATUSES = [("NEW", "Новая обартная связь"), ("PCD", "Обработано")]
 
-class ArticleAuthor(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    status_feed_back = models.CharField(
+        choices=STATUSES, max_length=3, verbose_name="Статус"
+    )
+
+    class Meta:
+        ordering = ["-date"]
+        verbose_name = "Обратная связь"
+        vebose_name_plural = "Обратная связь"
