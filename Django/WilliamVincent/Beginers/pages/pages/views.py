8233f7b1bf39d4ab00a7a3eb84d44f0e9d949093
metacortex687 <metacortex687@gmail.com>
2025-11-13 21:26:04 +0700

    Страница About

--- Django/WilliamVincent/Beginers/pages/pages/views.py
+++ Django/WilliamVincent/Beginers/pages/pages/views.py
@@ -5,4 +5,5 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
-# Create your views here.
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
