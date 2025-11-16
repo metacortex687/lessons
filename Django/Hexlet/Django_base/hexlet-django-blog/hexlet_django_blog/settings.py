a86e95527d7dc8af0ba1884459e8151417df74fa
metacortex687 <metacortex687@gmail.com>
2025-11-17 00:27:50 +0700

    начало переноса настроек в переменные окружения подгружаемых из  .env

--- Django/Hexlet/Django_base/hexlet-django-blog/hexlet_django_blog/settings.py
+++ Django/Hexlet/Django_base/hexlet-django-blog/hexlet_django_blog/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nz@q-=#drz+3209ioavm0x)osk8p_cunw^u!9!70nr0!9v_1+u'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = []
 
