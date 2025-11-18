ee64babe4f4cd956a6f3826a41956aa6df07b10c
metacortex687 <metacortex687@gmail.com>
2025-11-19 01:05:15 +0700

    настроил отображение списка комментариев

--- Django/Rusakov/makeup/blog/admin.py
+++ Django/Rusakov/makeup/blog/admin.py
@@ -4,6 +4,14 @@ from django.contrib import admin
 from blog.models import Article, Comment, Feedback, Image
 
 admin.site.register(Article)
-admin.site.register(Comment)
 admin.site.register(Feedback)
 admin.site.register(Image)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("date", "author", "article", "content")
+
+    search_fields = ('author',)
+
+    date_hierarchy = "date"
