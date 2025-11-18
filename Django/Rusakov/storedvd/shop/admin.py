6089a627693ab14b48dae6642bd625f53970fe2d
metacortex687 <metacortex687@gmail.com>
2025-11-18 23:27:17 +0700

    отображение списка в админке

--- Django/Rusakov/storedvd/shop/admin.py
+++ Django/Rusakov/storedvd/shop/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 from shop.models import Section,Product,Discount,Order,OrderLine
 
 admin.site.register(Section)
-admin.site.register(Product)
-admin.site.register(Discount)
 admin.site.register(Order)
 admin.site.register(OrderLine)
+
+class ProdactAdmin(admin.ModelAdmin):
+    list_display = ('title', 'section', 'imagr', 'price', 'date')
+    actions_on_bottom = True
+    list_per_page = 10
+    search_fields = ('title','cast')
+
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('code','value')
+
+admin.site.register(Product,ProdactAdmin)
+admin.site.register(Discount,DiscountAdmin)
+
+
