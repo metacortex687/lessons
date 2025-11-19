d1cd8bad4a415641dfbfcef9933d6895a5d0b71b
metacortex687 <metacortex687@gmail.com>
2025-11-19 21:05:02 +0700

    добавлен шаблон страницы и статические файлы

--- Django/Rusakov/storedvd/shop/views.py
+++ Django/Rusakov/storedvd/shop/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Section
+
+
+def index(request):
+    section = Section.objects.all().order_by("title")
+    return render(request, "index.html", context={"sections": section})
