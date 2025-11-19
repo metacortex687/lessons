bdf4a67a7548f7074cc08cf53fdd4471ebd70356
metacortex687 <metacortex687@gmail.com>
2025-11-19 18:04:53 +0700

    настроено отображение списков в админке

--- Django/Rusakov/makeup/blog/admin.py
+++ Django/Rusakov/makeup/blog/admin.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from blog.models import Article, Comment, Feedback, Image
 
-admin.site.register(Article)
-admin.site.register(Feedback)
+
 admin.site.register(Image)
 
 
@@ -12,6 +11,18 @@ admin.site.register(Image)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ("date", "author", "article", "content")
 
-    search_fields = ('author',)
+    search_fields = ("author",)
 
+    date_hierarchy = "date"
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_filter = ("status_feed_back",)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("id", "date", "author", "title")
+    
     date_hierarchy = "date"
