2f1f8aa855aa5d4466942ca5662df09db9f54931
metacortex687 <metacortex687@gmail.com>
2025-11-21 01:28:57 +0700

    feat: добавлен вывод фото, комментариев и заголовков статей

--- Django/Rusakov/makeup/blog/views.py
+++ Django/Rusakov/makeup/blog/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
 
+from .models import Image, Comment, Article
+
+
 def index(request):
-    return render(request, 'index.html')
+    images = Image.objects.all()
+    trim_comments = map(lambda x: trim_text(x.content, 150), Comment.objects.all()[:3])
+    articles = Article.objects.all().order_by("-date")[:3]
+
+    return render(
+        request,
+        "index.html",
+        context={"images": images, "trim_comments": trim_comments, "articles": articles},
+    )
+
+
+def trim_text(text: str, limit):
+    if len(text) < limit:
+        return text
+
+    cut_pos = text.rfind(" ", 0, limit)
+    return f"{text[:cut_pos]}..."
