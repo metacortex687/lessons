8718a32ef9e048d252c348eb2f4115f013ababba
metacortex687 <metacortex687@gmail.com>
2025-11-21 00:36:46 +0700

    feat: сортировка товаров

--- Django/Rusakov/storedvd/shop/views.py
+++ Django/Rusakov/storedvd/shop/views.py
@@ -6,5 +6,21 @@ from .models import Section, Product
 
 def index(request):
     section = Section.objects.all().order_by("title")
-    products = Product.objects.all()[:8]
-    return render(request, "index.html", context={"sections": section, 'products': products})
+    products = Product.objects.all().order_by(get_order_by_price(request))[:8]
+    return render(
+        request, "index.html", context={"sections": section, "products": products}
+    )
+
+def get_order_by_price(request):
+    order_by = ''
+    if request.GET.__contains__('sort') and request.GET.__contains__('up'):
+        sort = request.GET['sort']
+        up = request.GET['up']
+        if sort == 'price' or sort == 'title':
+            if up == '0':
+                order_by = '-'
+            order_by += sort 
+    if not order_by:
+        order_by = '-date'    
+
+    return order_by
