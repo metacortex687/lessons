5715efeac72750ffcc0c19ca225b249e7cc4af24
metacortex687 <metacortex687@gmail.com>
2025-11-23 00:01:07 +0700

    - страница о нас - страница контакты - страница со статьями - архив с навигацией - 9 случайных фото на странице статей и архива

--- Django/Rusakov/makeup/blog/urls.py
+++ Django/Rusakov/makeup/blog/urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('articles/', views.articles, name='articles'),
+    path('archive/<int:year>/<int:month>', views.archive, name='archive')
+]
--- Django/Rusakov/makeup/blog/views.py
+++ Django/Rusakov/makeup/blog/views.py
@@ -1,17 +1,25 @@
+from typing import Set
 from django.shortcuts import render
+from django.db.models import Count
 
 from .models import Image, Comment, Article
+import datetime
+import random
 
 
 def index(request):
     images = Image.objects.all()
     trim_comments = map(lambda x: trim_text(x.content, 150), Comment.objects.all()[:3])
-    articles = Article.objects.all().order_by("-date")[:3]
+    articles = Article.objects.all().order_by('-date')[:3]
 
     return render(
         request,
-        "index.html",
-        context={"images": images, "trim_comments": trim_comments, "articles": articles},
+        'index.html',
+        context={
+            'images': images,
+            'trim_comments': trim_comments,
+            'articles': articles,
+        },
     )
 
 
@@ -19,5 +27,57 @@ def trim_text(text: str, limit):
     if len(text) < limit:
         return text
 
-    cut_pos = text.rfind(" ", 0, limit)
-    return f"{text[:cut_pos]}..."
+    cut_pos = text.rfind(' ', 0, limit)
+    return f'{text[:cut_pos]}...'
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def articles(request):
+    articles_all = Article.objects.all().order_by('-date')
+    images = Image.objects.all()[:9]
+
+    return render(
+        request,
+        'articles.html',
+        context={
+            'articles': articles_all[:3]            
+            .annotate(comment_count=Count('comment')),
+            'archive_dates': sorted_list_archeve_dates(articles_all),
+            'images': images,
+        },
+    )
+
+
+def sorted_list_archeve_dates(articles_all):
+    archive_dates = set()
+    for article in articles_all:
+        archive_dates.add(datetime.date(article.date.year, article.date.month, 1))
+    archive_dates = list(archive_dates)
+    archive_dates = sorted(archive_dates)
+    print(archive_dates)
+    return archive_dates
+
+
+def archive(request, year, month):
+    articles_all = Article.objects.all()
+    images = Image.objects.all()[:9]
+
+    return render(
+        request,
+        'archive.html',
+        context={
+            'articles': articles_all.filter(date__year=year, date__month=month)
+            .order_by('-date')
+            .annotate(comment_count=Count('comment')),
+            'archive_dates': sorted_list_archeve_dates(articles_all),
+            'current_archive_date': datetime.date(year, month, 1),
+            'images': images,
+        },
+    )
