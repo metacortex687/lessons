5d42df848c459b615617c5422e423b0e684563fb
metacortex687 <metacortex687@gmail.com>
2025-11-26 01:00:01 +0700

    SimpleRouter для создания urls для API

--- Django/proproprogs/drf/drf_project/urls.py
+++ Django/proproprogs/drf/drf_project/urls.py
@@ -16,13 +16,18 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
 
 from women.views import WomenAPIList, WomenAPIUpdate, WomenAPIDetailView, WomenViewSet
+
+router =routers.SimpleRouter()
+router.register(r'women', WomenViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
-    path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
-    path('api/v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
+    path('api/v1/', include(router.urls)),
+    # path('api/v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
 ]
