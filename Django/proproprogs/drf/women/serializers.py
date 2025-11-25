f56fd52bc80e9a36166d877c14ded4930ac6ef9b
metacortex687 <metacortex687@gmail.com>
2025-11-26 00:07:08 +0700

    метод PUT с помощью сериализатора

--- Django/proproprogs/drf/women/serializers.py
+++ Django/proproprogs/drf/women/serializers.py
@@ -9,25 +9,32 @@ import io
 #         model = Women
 #         fields = ('title', 'cat_id')
 
+
 class WomenModel:
     def __init__(self, title, content):
         self.title = title
         self.content = content
 
+
 class WomenSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length = 255)
+    title = serializers.CharField(max_length=255)
     content = serializers.CharField()
     time_created = serializers.DateTimeField(read_only=True)
     time_update = serializers.DateTimeField(read_only=True)
-    is_published = serializers.BooleanField(default = True)
+    is_published = serializers.BooleanField(default=True)
     cat_id = serializers.IntegerField()
 
     def create(self, validated_data):
         return Women.objects.create(**validated_data)
 
-
-
-
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.content)
+        instance.time_update = validated_data.get('time_update', instance.time_update)
+        instance.is_published = validated_data.get('is_published', instance.is_published)
+        instance.cat_id = validated_data.get('cat_id', instance.cat_id)
+        instance.save()
+        return instance
 
 
 # def encode():
@@ -43,5 +50,3 @@ class WomenSerializer(serializers.Serializer):
 #     serializer = WomenSerializer(data=data)
 #     serializer.is_valid()
 #     print(serializer.validated_data)
-
-    
--- Django/proproprogs/drf/women/views.py
+++ Django/proproprogs/drf/women/views.py
@@ -12,19 +12,36 @@ from .models import Women
 #     queryset = Women.objects.all()
 #     serializer_class = WomenSerializer
 
+
 class WomenAPIView(APIView):
     def get(self, request):
         lst = Women.objects.all().values()
         return Response({'posts': WomenSerializer(lst, many=True).data})
-    
+
     def post(self, request):
-        serializer = WomenSerializer(data = request.data)
+        serializer = WomenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
         return Response({'post': serializer.data})
-    
+
     def delete(self, request):
         woman = get_object_or_404(Women, id=request.data['id'])
         woman.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'error': 'Method PUT not allowed'})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({'error': 'Object does not exists'})
+
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({'post': serializer.data})
