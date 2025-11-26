affd2b2cd93e35c968577b8f5cbc57d2e4adcb9a
metacortex687 <metacortex687@gmail.com>
2025-11-26 23:56:57 +0700

    Список, просмотр (CRUD)

--- Django/Hexlet/Django_base/hexlet-django-blog/hexlet_django_blog/article/urls.py
+++ Django/Hexlet/Django_base/hexlet-django-blog/hexlet_django_blog/article/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from hexlet_django_blog.article import views
 
 urlpatterns = [
-    path("<str:tags>/<int:article_id>/",views.index, name='article')
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:id>/', views.ArticleView.as_view(), name='article'),
 ]
--- Django/Hexlet/Django_base/hexlet-django-blog/hexlet_django_blog/article/views.py
+++ Django/Hexlet/Django_base/hexlet-django-blog/hexlet_django_blog/article/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
 
+from .models import Article
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html', context={'articles': articles})
+
+
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'articles/show.html', context={'article': article})
+
+# class ArticleComentsView(View):
+#     def get(self, request, *args, **kwargs):
+#         comment = ge
 
 # class ArticleHomeView(View):
 #     def get(self, request, *args, **kwargs):
 #         return HttpResponse("article ArticleHomeView 2")
 
+
 def index(request, tags, article_id):
-    return HttpResponse(f"Статья номер {article_id}. Тег {tags}")
-
-
-
-
-
+    return HttpResponse(f'Статья номер {article_id}. Тег {tags}')
