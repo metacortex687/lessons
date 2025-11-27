550328fe90d1ed41fe462f715c9db0779454c34b
metacortex687 <metacortex687@gmail.com>
2025-11-27 22:43:26 +0700

    Удаление (CRUD)

--- Django/Hexlet/Django_base/hexlet-django-blog/hexlet_django_blog/article/urls.py
+++ Django/Hexlet/Django_base/hexlet-django-blog/hexlet_django_blog/article/urls.py
@@ -5,6 +5,7 @@ from hexlet_django_blog.article import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'), #todo добавить articles_
     path('<int:id>/edit/', views.ArticleFormEditView.as_view(), name='articles_update'), #todo article переименовать в articles везде
+    path('<int:id>/delete/', views.ArticleFormDeleteView.as_view(), name='articles_delete'),
     path('<int:id>/', views.ArticleView.as_view(), name='article'),
     path('create/', views.ArticleCreateView.as_view(), name='article_create'),
 ]
