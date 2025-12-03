f8158e189df245f03f4edc9bf3da6683e2bda2b2
metacortex687 <metacortex687@gmail.com>
2025-12-03 23:13:45 +0700

    настройки аутентификации

--- Django/WilliamVincent/REST_API/blogapi/config/settings.py
+++ Django/WilliamVincent/REST_API/blogapi/config/settings.py
@@ -126,4 +126,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # 3rd party
-REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated']}
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
+    'DEFAULT_AUTHENTICATION_CLASSE': [
+        'rest_framework.autentication.SessionAuthentication',
+        'rest_framework.autentication.BasicAuthentication',
+    ],
+}
