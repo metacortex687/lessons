1b157a4397f00d43e9c08c3b146604409e3b06af
metacortex687 <metacortex687@gmail.com>
2025-12-03 22:36:59 +0700

    возможность смены пользователя на странице API

--- Django/WilliamVincent/REST_API/blogapi/posts/urls.py
+++ Django/WilliamVincent/REST_API/blogapi/posts/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from .views import PostList, PostDetail
 
 urlpatterns = [
     path('<int:pk>/', PostDetail.as_view()),
     path('', PostList.as_view()),
+    path('api-auth/',include('rest_framework.urls')),
 ]
