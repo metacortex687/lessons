ce7875566bb14ab5cf8f00bbbfdca9423a0cfc4a
metacortex687 <metacortex687@gmail.com>
2025-12-05 22:45:56 +0700

    свой роутер

--- Django/proproprogs/drf/drf_project/urls.py
+++ Django/proproprogs/drf/drf_project/urls.py
@@ -22,7 +22,21 @@ from rest_framework import routers
 
 from women.views import WomenAPIList, WomenAPIUpdate, WomenAPIDetailView, WomenViewSet
 
-router =routers.DefaultRouter()
+class MyCustomRouter(routers.SimpleRouter):
+    routes = [
+        routers.Route(url=r'^{prefix}$',
+                      mapping={'get': 'list'},
+                      name='{basename}-list',
+                      detail=False,
+                      initkwargs={'suffix': 'List'}),
+        routers.Route(url=r'^{prefix}/{lookup}$',
+                      mapping={'get': 'retrieve'},
+                      name='{basename}-detail',
+                      detail=True,
+                      initkwargs={'suffix': 'Detail'})
+    ]
+
+router = MyCustomRouter()
 router.register(r'women', WomenViewSet, basename='women')
 print(router.urls)
 
