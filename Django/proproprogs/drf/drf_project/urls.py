9e09d1b2a64365295dc923084b3941b79b3d20a9
metacortex687 <metacortex687@gmail.com>
2025-12-05 23:52:51 +0700

    добавить логин в API

--- Django/proproprogs/drf/drf_project/urls.py
+++ Django/proproprogs/drf/drf_project/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('api/v1/women/', WomenAPIList.as_view(), name='women_list'),
     path('api/v1/women/<int:pk>/', WomenAPIUpdate.as_view()),
     path('api/v1/womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
 ]
