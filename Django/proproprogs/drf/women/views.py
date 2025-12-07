0d072e7b77e8e91e76514091211b2ca749150e3d
metacortex687 <metacortex687@gmail.com>
2025-12-07 23:47:37 +0700

    свой класс пагинации

--- Django/proproprogs/drf/women/views.py
+++ Django/proproprogs/drf/women/views.py
@@ -10,12 +10,19 @@ from rest_framework.decorators import action
 from .serializers import WomenSerializer
 from .models import Women, Category
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
+from rest_framework import pagination
 
+
+class WomenAPIListPagination(pagination.PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'limit' #page_size 
+    max_page_size = 10000 #только на page_size_query_param и не влияет на page_size
 
 class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = WomenAPIListPagination
 
 
 class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
