acb33a6e38c3f9e98a71e58632e383fe60f9fd82
metacortex687 <metacortex687@gmail.com>
2025-12-11 23:45:57 +0700

    добавлен свой фильтр для склонения слова 'товар' в зависимости от колличества

--- Django/Rusakov/storedvd/shop/templatetags/my_filters.py
+++ Django/Rusakov/storedvd/shop/templatetags/my_filters.py
@@ -1,11 +1,24 @@
-
 from django import template
 
 register = template.Library()
 
+
 @register.filter(name='convert_play')
 def convert_play(value):
     hours = value // 3600
-    minutes = (value-hours*3600)//60
-    seconds = value-hours*3600-minutes*60
+    minutes = (value - hours * 3600) // 60
+    seconds = value - hours * 3600 - minutes * 60
     return f'{hours:02d}:{minutes:02d}:{seconds:02d}'
+
+
+@register.filter(name='diclention_of_products')
+def diclention_of_products(count):
+    suffix = ('товар', 'товара', 'товаров')
+    keys = (2, 0, 1, 1, 1, 2)
+    mod = count % 100
+    if 9 < mod < 20:
+        suffix_key = 2
+    else:
+        suffix_key = keys[min(mod % 10, 5)]
+
+    return suffix[suffix_key]
