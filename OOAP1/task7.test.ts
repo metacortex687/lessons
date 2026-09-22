d070d5229b2c6769515d3485cba7bb4fb696482f
metacortex687 <metacortex687@gmail.com>
2026-09-22 09:04:16 +0700

    Задание 7

---
+++ OOAP1/task7.test.ts
@@ -0,0 +1,97 @@
+import { describe, expect, test } from "vitest";
+import { createHash } from "node:crypto";
+import { HashTable, Hashable } from "./task7";
+
+// Запуск тестов: 
+// cd OOAP1
+// npm test -- task7.test.ts
+
+describe("HashTable вспомогательные тесты", () => {
+    test("HashTable строки", () => {
+        const ht = new HashTable<string>(5);
+        
+        expect(ht.is_empty()).toBe(true);
+        expect(ht.capacity()).toBe(5);
+
+        ht.put("q")
+
+        expect(ht.is_empty()).toBe(false);
+        expect(ht.count()).toBe(1);
+        expect(ht.get_put_status()).toBe("OK");
+
+        
+        ht.put("qw")
+        ht.put("qwe")
+        ht.put("ewq")
+        ht.put("qwert")
+
+        expect(ht.count()).toBe(5);
+        expect(ht.get_put_status()).toBe("OK");
+        expect(ht.find("ewq")).toBe(true);
+        expect(ht.find("ewq2")).toBe(false);
+
+
+        ht.put("ewq2")
+        expect(ht.get_put_status()).toBe("ERR_TABLE_FULL");
+        expect(ht.count()).toBe(5);
+
+        ht.remove("qwert");
+        expect(ht.get_remove_status()).toBe("OK");
+        expect(ht.count()).toBe(4);
+
+        ht.remove("ewq2");
+        expect(ht.get_remove_status()).toBe("ERR_NO_ELEMENT");
+        expect(ht.count()).toBe(4);    
+
+        ht.put("ewq2")
+        expect(ht.get_put_status()).toBe("OK");
+        expect(ht.count()).toBe(5);
+
+        ht.clear();
+        expect(ht.capacity()).toBe(5);
+        expect(ht.count()).toBe(0);
+
+    });
+
+
+    test("HashTable для Hashable объекта", () => {
+        const ht = new HashTable<CalendarEvent>(5);
+
+        const o1 = new CalendarEvent("Встреча",1);
+        const o2 = new CalendarEvent("Встреча",2);
+        const o3 = new CalendarEvent("Встреча",1);
+
+        ht.put(o1);        
+        expect(ht.find(o1)).toBe(true);
+
+        expect(ht.find(o2)).toBe(false);
+
+        expect(ht.find(o1)).toBe(true);        
+
+
+    });
+
+});
+
+
+class CalendarEvent extends Hashable {
+    constructor(
+        private readonly name: string,
+        private readonly time: number
+    ) {
+        super();
+    }
+
+    public override hash(): string {
+        return createHash("md5")
+            .update(JSON.stringify([this.name, this.time]))
+            .digest("hex");
+    }
+
+    public override equals(other: CalendarEvent): boolean {
+        return this.name === other.name && this.time === other.time;
+    }   
+    
+
+}
+
---
+++ OOAP1/task7.ts
@@ -0,0 +1,227 @@
+
+type PutStatus = "NIL" | "OK" | "ERR_TABLE_FULL";
+type RemoveStatus = "NIL" | "OK" | "ERR_NO_ELEMENT";
+
+// Здесь хранится ключ значение
+class StringHashTable<T extends string | Hashable> {
+
+    private _array:Array<T | undefined>;
+    private _count: number;
+
+    private _put_status: PutStatus = "NIL";
+    private _remove_status: RemoveStatus = "NIL";
+
+    // Конструктор:
+    // постусловие: создана пустая хэш-таблица
+    constructor(size: number) {
+        this._array = new Array<T | undefined>(size); 
+        this._count = 0; 
+    }
+
+    // Команды:
+
+    // предусловие: есть свободный слот или элемент уже содержится в таблице
+    // постусловие: добавлен или обновлен элемент
+    public put(key: string, value: T): void {
+        const index = this._seek_slot(key, value);
+
+        if (index === undefined) {
+            this._put_status =  "ERR_TABLE_FULL";
+            return;
+        }
+
+        if(this._array[index] !== value) {
+            this._count++;
+        }
+
+        this._put_status = "OK";
+        this._array[index] = value;
+
+    }
+
+
+    // предусловие: элемент есть в хэш таблице
+    // постусловие: удален элемент из таблицы
+    public remove(key: string, value: T): void {
+        const index = this._seek_slot(key, value);
+
+        if (index === undefined || this._array[index] === undefined) {
+            this._remove_status = "ERR_NO_ELEMENT";
+            return;
+        }
+
+        this._count--;
+        this._remove_status = "OK";    
+        this._array[index] = undefined;        
+    }
+
+    // постусловие: таблица пуста
+    public clear() {
+        this._count = 0;
+        this._array = new Array<T | undefined>(this.capacity());;
+    }
+
+    // Запросы:
+
+    public find(key: string, value: T): boolean {
+        const index = this._seek_slot(key, value);
+
+        if(index === undefined) {
+            return false
+        }
+
+        if(this._array[index] === undefined) {
+            return false
+        }
+
+        return true;
+    }
+
+
+    public count(): number {
+        return this._count;
+    }
+
+    is_empty(): boolean {
+        return this.count() === 0;
+    } //не уверен в названии
+
+    public capacity(): number {
+        return this._array.length;
+    }
+
+
+    // Дополнительные запросы статусов
+
+    get_put_status(): PutStatus {
+        return this._put_status;
+    }
+
+    get_remove_status(): RemoveStatus {
+        return this._remove_status;
+    }
+
+
+    // приватные методы
+    private _key_to_number(key: string): number {
+        const r  = 31;
+        const p  = 1000_000_007;
+
+        let res = 0;
+
+        for (const char of key) {
+            const code = char.codePointAt(0)!;
+            res = (res*r + code) % p;
+        }
+
+        return res;
+    } 
+
+    private _seek_slot(key: string, value: T): number | undefined {
+        let start_index = this._key_to_number(key) % this.capacity();
+
+        for (let i = 0; i < this.capacity(); i++)
+        {
+            const index = (start_index+i) % this.capacity();
+            if(this._array[index] === undefined || this._equals(this._array[index],value)) {
+                return index
+            }
+        }
+
+        return undefined;
+    } 
+
+    private _equals(a: T, b: T): boolean {
+        if(typeof a === "string" || typeof b === "string") {
+            return a === b
+        }
+
+        return a.equals(b);
+    }
+
+
+}
+
+abstract class Hashable {
+    abstract hash(): string;
+    abstract equals(other: Hashable): boolean;
+}
+
+class HashTable<T extends string | Hashable> {
+    private  _hash_table: StringHashTable<T>;
+
+    // Конструктор:
+    // постусловие: создана пустая хэш-таблица
+    constructor(size: number) {
+       this._hash_table = new StringHashTable<T>(size);
+    }   
+
+
+    //Команды:
+    
+    // предусловие: есть свободный слот или элемент уже содержится в таблице
+    // постусловие: добавлен или обновлен элемент
+    public put(value: T): void {
+        if (typeof value === "string") {
+            this._hash_table.put(value, value);
+        }
+        else {
+            this._hash_table.put(value.hash(), value);
+        }
+    }
+
+    // предусловие: элемент есть в хэш таблице
+    // постусловие: удален элемент из таблицы
+    public remove(value: T): void {
+        if (typeof value === "string") {
+            return this._hash_table.remove(value, value);
+        }
+        else {
+            return this._hash_table.remove(value.hash(), value);
+        }
+    }
+
+    // постусловие: таблица пуста
+    public clear() {
+        this._hash_table.clear();
+    }
+
+
+    // Запросы:
+
+    public find(value: T): boolean {
+        if (typeof value === "string") {
+            return this._hash_table.find(value, value);
+        }
+        else {
+            return this._hash_table.find(value.hash(), value);
+        }
+    }
+
+    public count(): number {
+        return this._hash_table.count();
+    }
+
+    public is_empty(): boolean {
+        return this._hash_table.is_empty();
+    }  
+
+    public capacity(): number {
+        return this._hash_table.capacity();
+    }
+
+
+    // Дополнительные запросы статусов
+
+    public get_put_status(): PutStatus {
+        return this._hash_table.get_put_status();
+    }
+
+    public get_remove_status(): RemoveStatus {
+        return this._hash_table.get_remove_status();
+    }
+
+}
+
+
+export {HashTable, Hashable}
